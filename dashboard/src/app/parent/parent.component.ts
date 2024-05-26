ceaa8bea1c980d9dd9958d8d9d45b90dd3293ebd
mustofakamal <mustofa.kamal@gmail.com>
2024-05-25 21:45:07 -0400

    adding weather component

--- dashboard/src/app/parent/parent.component.ts
+++ dashboard/src/app/parent/parent.component.ts
@@ -5,11 +5,12 @@ import { StockComponent } from '../stock/stock.component';
 import { EmailComponent } from '../email/email.component';
 import { TodoComponent } from '../todo/todo.component';
 import {LiveDateTimeComponent} from '../live-date-time/live-date-time.component'
+import { WeatherComponent } from '../weather/weather.component';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [CommonModule, FormsModule, StockComponent, EmailComponent, TodoComponent, LiveDateTimeComponent],
+  imports: [CommonModule, FormsModule, StockComponent, EmailComponent, TodoComponent, LiveDateTimeComponent, WeatherComponent],
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
@@ -18,4 +19,5 @@ export class ParentComponent {
   showEmail = true;
   showTodo = true;
   showDateTime=true;
+  showWeather=true;
 }
